fcfa1b09a19169fd9c8b47485e16c1ad987bd82e
ewpil <ewpil@WAGNER-XPS.attlocal.net>
2019-03-10 17:56:25 -0400

    completed create, read, write and backup config file routines

--- Tracker-Config/src/Amp.java
+++ Tracker-Config/src/Amp.java
@@ -9,15 +9,25 @@ public class Amp {
 		private String modelName;
 		private String comPort;
 		private String baudRate;
+		private String catPort1;
+		private String catBaud1;
+		private String catPort2;
+		private String catBaud2;
 		
-		public Amp(String modelName, String comPort, String baudRate) {
+		public Amp(String modelName, String comPort, String baudRate, String catPort1, String catBaud1, String catPort2, String catBaud2) {
 			this.modelName = modelName;
 			this.comPort = comPort;
 			this.baudRate = baudRate;
+			this.catPort1 = catPort1;
+			this.catBaud1 = catBaud1;
+			this.catPort2 = catPort2;
+			this.catBaud2 = catBaud2;
 		}
 
 		public void printInfo() {
-			System.out.println(modelName + "," + comPort + "-" + baudRate);
+			System.out.println(modelName + "," + comPort + "," + baudRate);
+			System.out.println(modelName + "," + catPort1 + "," + catBaud1);
+			System.out.println(modelName + "," + catPort2 + "," + catBaud2);
 			//
 		}
 }
--- Tracker-Config/src/app1.java
+++ Tracker-Config/src/app1.java
@@ -8,32 +8,46 @@ import java.util.Properties;
 public class app1 {
 	
 	//public configuration variables
-	public static String radio1ModelNumber;
-	public static String radio1ModelName;
-	public static String radio1PortName;
-	public static String radio1Baudrate;
-	public static String radio2ModelNumber;
-	public static String radio2ModelName;
-	public static String radio2PortName;
-	public static String radio2Baudrate;
-	public static String speAmpModelName;
-	public static String speAmpPortName;
-	public static String speAmpBaudrate;
-	public static String broadcastList;
-	public static String switch1IP;
-	public static String switch2IP;
-	public static String antLabelListR1;
-	public static String antLabelListR2;
-	public static String lastAntSelectR1;
-	public static String lastAntSelectR2;
+	// Radio 1 information
+	public static String radio1ModelNumber="";
+	public static String radio1ModelName="FTdx3000";
+	public static String radio1PortName="COM13";
+	public static String radio1Baudrate="38400";
+	// Radio 2 information
+	public static String radio2ModelNumber="";
+	public static String radio2ModelName="FT-991";
+	public static String radio2PortName="COM15";
+	public static String radio2Baudrate="38400";
+	// speAmp communications are currently not used by this program
+	// control port is used by the SPE Term_1.3K application
+	// CAT port 1 is connected to Win4Yaesu for radio 1
+	// CAT port 2 is not used at this time - 3/10/2019
+	public static String speAmpModelName="SPE 1.3K";
+	public static String speAmpPortName="COM3";  //control port
+	public static String speAmpBaudrate="4800";
+	public static String speAmpCATPortName1="COM2";  //CAT port radio 1
+	public static String speAmpCATBaudrate1="38400";
+	public static String speAmpCATPortName2="COM99";  //CAT port radio 2
+	public static String speAmpCATBaudrate2="38400";
+	// udp broadcast port information (defaults)
+	public static String broadcastList="LOCALHOST:127.0.0.1:12060, 13063, 13065; KB1RI-pi1:192.168.1.74:12060;";
+	// IP controlled antenna controller information
+	public static String switch1IP="192.168.1.x";
+	public static String switch2IP="192.168.1.y";
+	public static String antLabelListR1="";
+	public static String antLabelListR2="";
+	public static String lastAntSelectR1="";
+	public static String lastAntSelectR2="";
 		
 
 	public static void main(String[] args) {
 		
 		// Initialize program configuration information from config file
-		String configFilename = "AntennaTrackerCopy.cfg"; 
+		String configPath = "C:\\Users\\ewpil\\iCloudDrive\\eclipse-workspace\\";
+		String configFilename = "AntennaTracker.cfg"; 
+		String configFile = configPath.concat(configFilename);
 		
-		readConfigFile(configFilename);
+		readConfigFile(configFile);
 		
 		//verification using println
 		
@@ -41,7 +55,9 @@ public class app1 {
 		
 		System.out.println("Radio1: " + radio1ModelNumber + "," + radio1ModelName + "," + radio1PortName + "," + radio1Baudrate);
 		System.out.println("Radio2: " + radio2ModelNumber + "," + radio2ModelName + "," + radio2PortName + "," + radio2Baudrate);
-		System.out.println("AMP: " + speAmpModelName + "," + speAmpPortName + "," + speAmpBaudrate);
+		System.out.print("AMP: " + speAmpModelName + "," + speAmpPortName + "," + speAmpBaudrate);
+		System.out.print("," + speAmpCATPortName1 + "," + speAmpCATBaudrate1);
+		System.out.println("," + speAmpCATPortName2 + "," + speAmpCATBaudrate2);
 		
 		System.out.println("HostList: " + broadcastList);
 		
@@ -60,17 +76,22 @@ public class app1 {
 		
 		Radio radio2 = new Radio(radio2ModelNumber, radio2ModelName, radio2PortName, radio2Baudrate);
 		
-		Amp amp1 = new Amp(speAmpModelName, speAmpPortName, speAmpBaudrate);
+		Amp amp1 = new Amp(speAmpModelName, speAmpPortName, speAmpBaudrate, speAmpCATPortName1, speAmpCATBaudrate1, speAmpCATPortName2, speAmpCATBaudrate2);
 		
 		// simple test of the class/objects and their info method 
 		radio1.printInfo();
 		radio2.printInfo();
 		amp1.printInfo();
 		
+		// backup configuration file
+		System.out.println("Backing up configuration file...");
+		configFile = configPath.concat(configFilename); 
+		backupConfigFile(configFile);
+		
 		// write a copy of the configuration file as a test
 		System.out.println("Writing copy of configuration file...");
-		configFilename = "AntennaTrackerCopy.cfg"; 
-		writeConfigFile(configFilename);
+		configFile = configPath.concat(configFilename); 
+		writeConfigFile(configFile);
 		System.out.println("Writing completed");
 		
 	}
@@ -78,7 +99,7 @@ public class app1 {
 	public static void readConfigFile(String file) {
 	
 		//This code sets up the File Reader Object and points to the config file.
-		try(FileReader reader = new FileReader("C:\\Users\\ewpil\\iCloudDrive\\eclipse-workspace\\"+file)) {
+		try(FileReader reader = new FileReader(file)) {
 			Properties properties = new Properties();
 			properties.load(reader);
 			
@@ -97,6 +118,10 @@ public class app1 {
 			speAmpModelName = properties.getProperty("speAmpModelName");
 			speAmpPortName = properties.getProperty("speAmpPortName");
 			speAmpBaudrate = properties.getProperty("speAmpBaudrate");
+			speAmpCATPortName1 = properties.getProperty("speAmpCATPortName1");
+			speAmpCATBaudrate1 = properties.getProperty("speAmpCATBaudrate1");
+			speAmpCATPortName2 = properties.getProperty("speAmpCATPortName2");
+			speAmpCATBaudrate2 = properties.getProperty("speAmpCATBaudrate2");
 			
 			broadcastList = properties.getProperty("broadcastList");
 			
@@ -111,13 +136,12 @@ public class app1 {
 			
 			
 		} catch (Exception e) {
-			;
 			e.printStackTrace();
 		}
 	}
 	
 	public static void writeConfigFile(String file) {
-		File fileInput = new File("C:\\Users\\ewpil\\iCloudDrive\\eclipse-workspace\\"+file);
+		File fileInput = new File(file);
 		FileWriter fileWriter;
 		try {
 			fileWriter = new FileWriter(fileInput);
@@ -125,7 +149,7 @@ public class app1 {
 			bufferWrite.append("# java properties file for my AntennaTracker program project\r\n\r\n");
 			bufferWrite.append("# Radio & accessory model information\r\n");
 			bufferWrite.append("radio1ModelNumber=" + radio1ModelNumber + "\r\n");
-			bufferWrite.append("radio1ModelNumber=" + radio1ModelName + "\r\n");
+			bufferWrite.append("radio1ModelName=" + radio1ModelName + "\r\n");
 			bufferWrite.append("radio1PortName=" + radio1PortName + "\r\n");
 			bufferWrite.append("radio1Baudrate=" + radio1Baudrate + "\r\n");
 			bufferWrite.newLine();
@@ -134,9 +158,17 @@ public class app1 {
 			bufferWrite.append("radio2PortName=" + radio2PortName + "\r\n");
 			bufferWrite.append("radio2Baudrate=" + radio2Baudrate + "\r\n");
 			bufferWrite.newLine();
+			bufferWrite.append("# speAmp communications are currently not used by this program\r\n" + 
+					"	# control port is used by the SPE Term_1.3K application\r\n" + 
+					"	# CAT port 1 is connected to Win4Yaesu for radio 1\r\n" + 
+					"	# CAT port 2 is not used at this time - 3/10/2019\r\n");
 			bufferWrite.append("speAmpModelName=" + speAmpModelName + "\r\n");
 			bufferWrite.append("speAmpPortName=" + speAmpPortName + "\r\n");
 			bufferWrite.append("speAmpBaudrate=" + speAmpBaudrate + "\r\n");
+			bufferWrite.append("speAmpCATPort1=" + speAmpCATPortName1 + "\r\n");
+			bufferWrite.append("speAmpCATBaud=" + speAmpCATBaudrate1 + "\r\n");
+			bufferWrite.append("speAmpCATPortName2=" + speAmpCATPortName2 + "\r\n");
+			bufferWrite.append("speAmpCATBaudrate2=" + speAmpCATBaudrate2 + "\r\n");
 			bufferWrite.newLine();
 			bufferWrite.append("# broadcastList format \"Hostname:ip-address:port list(comma separated);\"\r\n" + 
 					"#\r\n" + 
@@ -173,50 +205,89 @@ public class app1 {
 		// create a new config backup file if one doesn't exist
 		
 		// initialize File object
-		File origFile = new File("C:\\Users\\ewpil\\iCloudDrive\\eclipse-workspace\\"+file);
+		File origFile = new File(file);
 		
-		// check if file exists
-		if(origFile.exists()) {
-			// read the file
-			readConfigFile(file);
+		String backupFilename = file.substring(0,file.length()-3) + "bak";
 		
-			// now write a copy
-			writeConfigFile(file+".bak");
+		File backupFile = new File(backupFilename);
 		
-			System.out.println("File backup complete");
-			returnVal = true;
-			return returnVal;
-			
-		} else {
-			System.out.println("File does not exist");
-		
-			//if file doesn't exist create it
-			// initialize the File object
-			File newFile = new File("C:\\Users\\ewpil\\iCloudDrive\\eclipse-workspace\\"+file);
-			
-			boolean result;
-			try {
-				// create new empty file with default values
-				result = newFile.createNewFile();
+		try {
+			// check if file exists
+			if(origFile.exists()) {
 				
-				// now write default values to the config file
+				if(backupFile.exists()) {
+					// delete current backupfile
+					boolean result = backupFile.delete();
+					
+					if(result) {
+						System.out.println("Successfully deleted: " + backupFile.getCanonicalPath());
+					} else {
+						System.out.println("Failed deleting " + backupFile.getCanonicalPath());
+					}
+				}
 				
-				// APPENDS GO HERE!!!
-				
-				// test if successfully created a new file
+				// rename the file if it exists
+				boolean result = origFile.renameTo(backupFile);
+				// check if the rename operation is success
 				if(result) {
+					
+					// now read the newly created backup file
+					readConfigFile(backupFilename);
+				
+					// write a new version of the configuration file
+					writeConfigFile(file);
+				
+					System.out.println("Configuration file backup complete");
+	
 					returnVal = true;
-					System.out.println("Successfully created " + newFile.getCanonicalFile());
-				}
-				else {
-					System.out.println("Failed creating " + newFile.getCanonicalPath());		
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
+					return returnVal;
+				
+				} else {
+					System.out.println("Configuration file backup failed");
+					return returnVal;
+				}	
+			
+			} else {
+				System.out.println("Configuration file does not exist");
+			
+				// if file doesn't exist create it
+				createConfigFile(file);
 			}
-			return returnVal;
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+		return returnVal;
 	}
 
+	public static boolean createConfigFile(String file) {	
+		
+		boolean returnVal = false;
+		// initialize the File object
+		File newFile = new File(file);
+		
+		boolean result;
+		try {
+			// create new empty file with default values
+			result = newFile.createNewFile();
+			
+			// now write default values to the config file
+			
+			// APPENDS GO HERE!!!
+			System.out.println("Creating config file from defaults");
+			
+			// test if successfully created a new file
+			if(result) {
+				returnVal = true;
+				System.out.println("Successfully created " + newFile.getCanonicalFile());
+			}
+			else {
+				returnVal = false;
+				System.out.println("Failed creating " + newFile.getCanonicalPath());		
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return returnVal;
+	} // end of createConfigFile
 }
 
